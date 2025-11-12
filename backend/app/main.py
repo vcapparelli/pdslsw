889b191a7af549d0f182195e79062594a46e5dc7
Vítor Capparelli <vcapparelli@riseup.net>
2025-11-12 03:08:41 -0300

    Deploy do Backend

---
+++ backend/app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, HTTPException
+import os, asyncpg
+
+
+app = FastAPI()
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgres://postgres:password@db:5432/postgres')
+
+
+async def get_pool():
+    return await asyncpg.create_pool(DATABASE_URL)
+
+
+@app.get("/softwares")
+async def read_softwares():
+    try:
+        pool = await get_pool()
+        async with pool.acquire() as conn:
+            query = "SELECT id, name FROM softwares ORDER BY id"
+            rows = await conn.fetch(query)
+            return [ {"id": r["id"], "name": r["name"]} for r in rows]
+    except Exception:
+        raise HTTPException(status_code=500, detail="Erro ao conectar com o Banco de Dados")
