a7da085d534421eab5b604dc60f7a1c87a028461
Vítor Capparelli <vitorcapparelli@prefeitura.sp.gov.br>
2025-11-12 19:09:55 -0300

    Cria conteúdo do Frontend (HTML + JS)

---
+++ frontend/public/index.js
@@ -0,0 +1,17 @@
+async function load(){
+  const ul = document.getElementById("list");
+  ul.innerHTML = "Carregando...";
+  try {
+    const res = await fetch("http://localhost:8000/softwares");
+    if (!res.ok) throw new Error("HTTP " + res.status);
+    const softwares = await res.json();
+    ul.innerHTML = "";
+    softwares.forEach(s => {
+      const li = document.createElement("li");
+      li.textContent = "${s.id}: ${s.name}";
+      ul.appendChild(li);
+    });
+  } catch (e) {
+    ul.innerHTML = "Erro: " + e.message;
+  }
+}
